7e041deaa9fb94ef83588a14847fe032206d034e
ashgaming <68996995+ashgaming@users.noreply.github.com>
2025-05-10 22:27:01 +0530

    fix2

--- nextfrontend/app/layout.jsx
+++ nextfrontend/app/layout.jsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-adsense-account" content="ca-pub-6282336656230210"></meta>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-XVF639PHFD"
           strategy="afterInteractive"
